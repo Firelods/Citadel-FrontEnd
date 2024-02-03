8ec38fc70b57c6fd3218274f82f427c4238323ba
Firelods <clement.lefevre1@etu.unice.fr>
2024-02-03 16:14:20 +0100

    🩹 fix angular 17 syntax

--- src/app/logs-panel/logs-panel.component.ts
+++ src/app/logs-panel/logs-panel.component.ts
@@ -1,26 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
-import {SocketService} from "../socket.service";
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../socket.service';
 
 @Component({
   selector: 'app-logs-panel',
   standalone: true,
-  imports: [
-    NgForOf,
-    NgIf
-  ],
+  imports: [],
   templateUrl: './logs-panel.component.html',
-  styleUrl: './logs-panel.component.scss'
+  styleUrl: './logs-panel.component.scss',
 })
 export class LogsPanelComponent implements OnInit {
   messages: string[] = [];
   SocketService: SocketService;
 
-  constructor(SocketService : SocketService) {
+  constructor(SocketService: SocketService) {
     this.SocketService = SocketService;
   }
 
   ngOnInit(): void {
+    //TODO: Change this construction
     this.listenDisplayPlayerStartPlaying();
     this.listenDisplayPlayerPickCards();
     this.listenDisplayPlayerPicksGold();
@@ -63,57 +60,75 @@ export class LogsPanelComponent implements OnInit {
   }
 
   listenDisplayPlayerStartPlaying() {
-    this.SocketService.listen('displayPlayerStartPlaying').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerStartPlaying').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerPickCards() {
-    this.SocketService.listen('displayPlayerPickCards').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerPickCards').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerPicksGold() {
-    this.SocketService.listen('displayPlayerPicksGold').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerPicksGold').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerChooseCharacter() {
-    this.SocketService.listen('displayPlayerChooseCharacter').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerChooseCharacter').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerRevealCharacter() {
-    this.SocketService.listen('displayPlayerRevealCharacter').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerRevealCharacter').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerUseCondottiereDistrict() {
-    this.SocketService.listen('displayPlayerUseCondottiereDistrict').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerUseCondottiereDistrict').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerScore() {
-    this.SocketService.listen('displayPlayerScore').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerScore').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerGetBonus() {
-    this.SocketService.listen('displayPlayerGetBonus').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerGetBonus').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerUseAssassinEffect() {
-    this.SocketService.listen('displayPlayerUseAssassinEffect').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerUseAssassinEffect').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerInfo() {
@@ -123,15 +138,19 @@ export class LogsPanelComponent implements OnInit {
   }
 
   listenDisplayUnusedCharacterInRound() {
-    this.SocketService.listen('displayUnusedCharacterInRound').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayUnusedCharacterInRound').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayGameFinished() {
-    this.SocketService.listen('displayGameFinished').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayGameFinished').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayRound() {
@@ -141,67 +160,89 @@ export class LogsPanelComponent implements OnInit {
   }
 
   listenDisplayPlayerError() {
-    this.SocketService.listen('displayPlayerError').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerError').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerStrategy() {
-    this.SocketService.listen('displayPlayerStrategy').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerStrategy').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayStolenCharacter() {
-    this.SocketService.listen('displayStolenCharacter').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayStolenCharacter').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayActualNumberOfGold() {
-    this.SocketService.listen('displayActualNumberOfGold').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayActualNumberOfGold').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerUseMagicianEffect() {
-    this.SocketService.listen('displayPlayerUseMagicianEffect').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerUseMagicianEffect').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerHasGotObservatory() {
-    this.SocketService.listen('displayPlayerHasGotObservatory').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerHasGotObservatory').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerUseThiefEffect() {
-    this.SocketService.listen('displayPlayerUseThiefEffect').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerUseThiefEffect').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerDiscardCard() {
-    this.SocketService.listen('displayPlayerDiscardCard').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerDiscardCard').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerUseLaboratoryEffect() {
-    this.SocketService.listen('displayPlayerUseLaboratoryEffect').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerUseLaboratoryEffect').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayPlayerUseManufactureEffect() {
-    this.SocketService.listen('displayPlayerUseManufactureEffect').subscribe((data: string) => {
-      this.addMessage(data);
-    });
+    this.SocketService.listen('displayPlayerUseManufactureEffect').subscribe(
+      (data: string) => {
+        this.addMessage(data);
+      }
+    );
   }
 
   listenDisplayGoldCollectedFromDisctrictType() {
-    this.SocketService.listen('displayGoldCollectedFromDisctrictType').subscribe((data: string) => {
+    this.SocketService.listen(
+      'displayGoldCollectedFromDisctrictType'
+    ).subscribe((data: string) => {
       this.addMessage(data);
     });
   }
